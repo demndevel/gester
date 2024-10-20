690940c52d66ca175ce78d10019913125e9b5c30
demn <demndevel@gmail.com>
2024-10-20 18:04:43 +0300

    feat(domain): implement latinizing to make correct sorting when there is cyrillic results

--- domain/src/main/java/com/demn/domain/usecase/OperationResultSorterUseCase.kt
+++ domain/src/main/java/com/demn/domain/usecase/OperationResultSorterUseCase.kt
@@ -9,6 +9,9 @@ import com.demn.plugincore.operationresult.OperationResult
 import com.demn.plugincore.operationresult.ResultType
 import com.frosch2010.fuzzywuzzy_kotlin.FuzzySearch
 import com.frosch2010.fuzzywuzzy_kotlin.ToStringFunction
+import com.michaeltroger.latintocyrillic.Alphabet
+import com.michaeltroger.latintocyrillic.LatinCyrillicFactory
+import kotlinx.coroutines.runBlocking
 
 interface OperationResultSorterUseCase {
     suspend operator fun invoke(
@@ -26,12 +29,18 @@ class MockOperationResultSorterUseCase : OperationResultSorterUseCase {
 }
 
 class OperationResultFuzzySearcherToString : ToStringFunction<OperationResult> {
-    override fun apply(item: OperationResult): String {
-        if (item is CommandOperationResult) return item.name
-        if (item is BasicOperationResult) return item.text
-        if (item is IconOperationResult) return item.text
-        return ""
+    override fun apply(item: OperationResult): String = runBlocking {
+        if (item is CommandOperationResult) return@runBlocking latinise(item.name)
+        if (item is BasicOperationResult) return@runBlocking latinise(item.text)
+        if (item is IconOperationResult) return@runBlocking latinise(item.text)
+        return@runBlocking ""
     }
+}
+
+private suspend fun latinise(input: String): String {
+    val latinCyrillic = LatinCyrillicFactory.create(Alphabet.RussianIso9)
+
+    return if (latinCyrillic.isCyrillic(input)) latinCyrillic.cyrillicToLatin(input) else input
 }
 
 class OperationResultSorterUseCaseImpl(
@@ -46,12 +55,12 @@ class OperationResultSorterUseCaseImpl(
         return sortByFrecency(input, fuzzySorted)
     }
 
-    private fun fuzzySort(
+    private suspend fun fuzzySort(
         input: String,
         results: List<OperationResult>
     ): List<OperationResult> {
         val fuzzyExtracted = FuzzySearch.extractAll(
-            query = input,
+            query = latinise(input),
             choices = results,
             toStringFunction = OperationResultFuzzySearcherToString()
         )
